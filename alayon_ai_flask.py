26462d330f9a9be2b624f782a6d30ee4cde4bae8
jaybee <88524940+bugtech-systems@users.noreply.github.com>
2025-05-18 23:37:01 +0800

    Fix files

--- alayon_ai_flask.py
+++ alayon_ai_flask.py
@@ -5,8 +5,8 @@ import os
 import threading
 import time
 from flask import Flask, request, jsonify
-# from langchain_ollama import OllamaLLM
-from langchain_community.llms import ollama
+from langchain_ollama import OllamaLLM
+# from langchain_community.llms import ollama
 
 from langchain_core.prompts import PromptTemplate
 from watchdog.observers import Observer
@@ -53,7 +53,7 @@ def create_llm_from_config():
 
     print("[INFO] Using LLM config:", model_config)
 
-    return ollama.Ollama(
+    return OllamaLLM(
         model=model_name,
         temperature=temperature,
         top_p=top_p
