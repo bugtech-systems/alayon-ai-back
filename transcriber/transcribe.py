78690fc75141d8867ab8f34ba8900cf28b31554a
jaybee <88524940+bugtech-systems@users.noreply.github.com>
2025-05-21 14:13:15 +0800

    added

--- transcriber/transcribe.py
+++ transcriber/transcribe.py
@@ -3,6 +3,6 @@ import sys
 import whisper
 
 audio_file = sys.argv[1]
-model = whisper.load_model("base")
+model = whisper.load_model("large")
 result = model.transcribe(audio_file)
 print(result["text"])
