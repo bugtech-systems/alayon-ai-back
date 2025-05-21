75462dc63e1813c9e1219baf33f12eefaca67091
jaybee <88524940+bugtech-systems@users.noreply.github.com>
2025-05-21 14:27:19 +0800

    added

--- transcribe.py
+++ transcribe.py
@@ -1,21 +1,8 @@
 # transcriber/transcribe.py
 import sys
 import whisper
-from spellchecker import SpellChecker
-spell = SpellChecker()
 
 audio_file = sys.argv[1]
 model = whisper.load_model("large")
 result = model.transcribe(audio_file, language="en")
-
-
-
-words = result['text'].split()
-misspelled = spell.unknown(words)
-for word in misspelled:
-    print(f"{word} → {spell.correction(word)}")
-
-
 print(result["text"])
-
-
